7ddebe6568256d193ddff27fa0947a52a4a6ee04
DHRUTIK PATEL <dhrutikpatel2017@gmail.com>
2021-10-20 20:20:39 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*--------------------------------  Responsive NavBar --------------------------------*/
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
